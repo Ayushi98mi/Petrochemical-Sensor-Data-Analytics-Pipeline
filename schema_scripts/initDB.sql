7866425f7c2e7d6e32c3ec1d808114d61ce78f83
Ayushi98mi <ayushib306@gmail.com>
2026-07-31 11:11:07 +0530

    Create initDB.sql

---
+++ schema_scripts/initDB.sql
@@ -0,0 +1,42 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'PetrochemicalAnalytics' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'Bronze', 'Silver', and 'Gold'.
+	
+WARNING:
+    Running this script will drop the entire 'PetrochemicalAnalytics' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'PetrochemicalAnalytics')
+BEGIN
+    ALTER DATABASE PetrochemicalAnalytics SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE PetrochemicalAnalytics;
+END;
+GO
+
+-- Create the 'PetrochemicalAnalytics' database
+CREATE DATABASE PetrochemicalAnalytics;
+GO
+
+USE PetrochemicalAnalyticse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA Bronze;
+GO
+
+CREATE SCHEMA Silver;
+GO
+
+CREATE SCHEMA Gold;
+GO
